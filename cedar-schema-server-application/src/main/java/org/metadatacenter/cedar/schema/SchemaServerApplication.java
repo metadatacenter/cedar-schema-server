2322206e82ffc76fc9f76bb2497e88bc4478bfd4
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-02-09 22:21:13 -0800

    Alpha release of new permission indexing feature

--- cedar-schema-server-application/src/main/java/org/metadatacenter/cedar/schema/SchemaServerApplication.java
+++ cedar-schema-server-application/src/main/java/org/metadatacenter/cedar/schema/SchemaServerApplication.java
@@ -1,17 +1,12 @@
 package org.metadatacenter.cedar.schema;
 
-import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import org.metadatacenter.bridge.CedarDataServices;
 import org.metadatacenter.cedar.schema.health.SchemaServerHealthCheck;
 import org.metadatacenter.cedar.schema.resources.IndexResource;
-import org.metadatacenter.cedar.util.dw.CedarDropwizardApplicationUtil;
-import org.metadatacenter.config.CedarConfig;
+import org.metadatacenter.cedar.util.dw.CedarMicroserviceApplication;
 
-public class SchemaServerApplication extends Application<SchemaServerConfiguration> {
-
-  protected static CedarConfig cedarConfig;
+public class SchemaServerApplication extends CedarMicroserviceApplication<SchemaServerConfiguration> {
 
   public static void main(String[] args) throws Exception {
     new SchemaServerApplication().run(args);
@@ -19,25 +14,19 @@ public class SchemaServerApplication extends Application<SchemaServerConfigurati
 
   @Override
   public String getName() {
-    return "schema-server";
+    return "cedar-schema-server";
   }
 
   @Override
-  public void initialize(Bootstrap<SchemaServerConfiguration> bootstrap) {
-    cedarConfig = CedarConfig.getInstance();
-    CedarDataServices.getInstance(cedarConfig);
-
-    CedarDropwizardApplicationUtil.setupKeycloak();
+  public void initializeApp(Bootstrap<SchemaServerConfiguration> bootstrap) {
   }
 
   @Override
-  public void run(SchemaServerConfiguration configuration, Environment environment) {
+  public void runApp(SchemaServerConfiguration configuration, Environment environment) {
     final IndexResource index = new IndexResource();
     environment.jersey().register(index);
 
     final SchemaServerHealthCheck healthCheck = new SchemaServerHealthCheck();
     environment.healthChecks().register("message", healthCheck);
-
-    CedarDropwizardApplicationUtil.setupEnvironment(environment);
   }
 }
