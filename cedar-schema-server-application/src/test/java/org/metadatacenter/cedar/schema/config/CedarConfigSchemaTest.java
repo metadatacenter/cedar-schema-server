a8c41fa305ecc8ce8d8bc47148884f0d8c14ba35
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-06-13 11:16:10 -0700

    ENV vars added to tests

--- cedar-schema-server-application/src/test/java/org/metadatacenter/cedar/schema/config/CedarConfigSchemaTest.java
+++ cedar-schema-server-application/src/test/java/org/metadatacenter/cedar/schema/config/CedarConfigSchemaTest.java
@@ -20,6 +20,8 @@ public class CedarConfigSchemaTest {
 
     env.put(CedarEnvironmentVariable.CEDAR_HOST.getName(), "metadatacenter.orgx");
 
+    env.put(CedarEnvironmentVariable.CEDAR_NET_GATEWAY.getName(), "127.0.0.1");
+
     env.put(CedarEnvironmentVariable.CEDAR_MONGO_APP_USER_NAME.getName(), "cedarUser");
     env.put(CedarEnvironmentVariable.CEDAR_MONGO_APP_USER_PASSWORD.getName(), "password");
     env.put(CedarEnvironmentVariable.CEDAR_MONGO_HOST.getName(), "localhost");
@@ -28,6 +30,8 @@ public class CedarConfigSchemaTest {
     env.put(CedarEnvironmentVariable.CEDAR_SCHEMA_HTTP_PORT.getName(), "9003");
     env.put(CedarEnvironmentVariable.CEDAR_SCHEMA_ADMIN_PORT.getName(), "9103");
     env.put(CedarEnvironmentVariable.CEDAR_SCHEMA_STOP_PORT.getName(), "9203");
+
+    env.put(CedarEnvironmentVariable.CEDAR_USER_HTTP_PORT.getName(), "9005");
 
     TestUtil.setEnv(env);
   }
